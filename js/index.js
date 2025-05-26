a09467318aec7f15003a8eb38a579b00596be165
bryanrico7360 <bencraft360@gmail.com>
2025-05-25 21:46:40 -0500

    Se añaden estilos y tarjetas del main, de tal manera que se peuda acceder desde la base de datos

--- js/index.js
+++ js/index.js
@@ -3,17 +3,23 @@ async function getSites() {
 
 const sites = document.getElementById("sites")
 
-const response = await fetch("");
+const response = await fetch("https://programacion-web-backend.onrender.com/api/sites");
 const data = await response.json();
 
-sites.array.forEach(site => {
+sites.forEach(site => {
   const container = document.createElement("div");
 
-  container.innerHTM = ``;
+  container.innerHTML = `<div class="site">
+  <h3>${site.name}</h3>
+  <p>${site.descripcion}</p>
+  <img src="${site.imagen}"</>
+  <p class="rating">★ ${site.calificacion}</p>`;
 
 
   sites.appendChild(container);
 });
+
+getSites();
 
 }
 
